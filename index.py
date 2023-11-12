7a9cb0b85bea1be791daca9202ef6bdcb46c27b2
marcelo <marcelomiranda200@hotmail.com>
2023-11-12 16:36:00 -0300

    tradução do codigo

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
-# index.py
-
+#
 class Task:
     def __init__(self, description, due_date, priority):
         self.description = description
         self.due_date = due_date
         self.priority = priority
 
-
+# Abaixo está uma Função para validar a prioridade
 def validate_priority(priority):
-    # Função para validar a prioridade
     return 1 <= priority <= 5
 
 
+# Abaixo está o controlador das tarefas
 class TaskController:
     tasks = []
 
@@ -20,40 +19,40 @@ class TaskController:
         cls.tasks.append(task)
 
     @classmethod
+     # Função para visualizar as tarefas
     def view_tasks(cls):
-        # Função para visualizar as tarefas
         for task in cls.tasks:
             print(f"Description: {task.description}, Due Date: {task.due_date}, Priority: {task.priority}")
 
-    # Implemente as outras operações do CRUD conforme necessário
+   
 
-
+# Abaixo é uma interface para criação das tarefas e prioridades
 class ConsoleUI:
     @staticmethod
     def get_task_input():
-        description = input("Enter task description: ")
-        due_date = input("Enter due date: ")
-        priority = int(input("Enter priority (1-5): "))
+        description = input("Digite a descrição da tareda: ")
+        due_date = input("Digite a Data: ")
+        priority = int(input("Digite a Prioridade (de 1 a 5): "))
         return Task(description, due_date, priority)
 
 
 def main():
     while True:
-        print("\n1. Add Task\n2. View Tasks\n3. Exit")
-        choice = input("Enter your choice: ")
+        print("\n1. Adicionar tarefa\n2. Ver tarefas\n3. sair")
+        choice = input("Digite sua Escolha: ")
 
         if choice == "1":
             task = ConsoleUI.get_task_input()
             if validate_priority(task.priority):
                 TaskController.add_task(task)
             else:
-                print("Invalid priority. Priority must be between 1 and 5.")
+                print("Priorização inválida, você deve escolher algo entre 1 e 5.")
         elif choice == "2":
             TaskController.view_tasks()
         elif choice == "3":
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Escolha inválida. Tente Novamente")
 
 
 if __name__ == "__main__":
