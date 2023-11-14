b2f312ffcca1b78547448cead2503cb00707ee4b
marcelo <marcelomiranda200@hotmail.com>
2023-11-13 21:46:24 -0300

    inserção de mais comentários

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #
 class Task:
+    # O método __init__ é o construtor da classe Task
     def __init__(self, description, due_date, priority):
         self.description = description
         self.due_date = due_date
@@ -12,14 +13,15 @@ def validate_priority(priority):
 
 # Abaixo está o controlador das tarefas, para funções de cadastrar tarefas e ver tarefas
 class TaskController:
+    # Lista/array para armazenar as tarefas
     tasks = []
-    #Função para cadastro de tarefas
+    #Função/método para cadastro de tarefas na lista de tarefas
     @classmethod
     def add_task(cls, task):
         cls.tasks.append(task)
 
     @classmethod
-     # Função para visualizar as tarefas, depois de já feitas
+     # Função/método para visualizar as tarefas, depois de já cadastradas
     def view_tasks(cls):
         for task in cls.tasks:
             print(f"Descrição: {task.description}, Data de Vencimento: {task.due_date}, Prioridade: {task.priority}")
@@ -35,7 +37,7 @@ class ConsoleUI:
         priority = int(input("Digite a Prioridade (de 1 a 5): "))
         return Task(description, due_date, priority)
 
-
+#aqui é a inicialização principal do programa
 def main():
     while True:
         print("\n1. Adicionar tarefa\n2. Ver tarefas\n3. sair")
@@ -54,6 +56,71 @@ def main():
         else:
             print("Escolha inválida. Tente Novamente")
 
+class Task:
+    # O método __init__ é o construtor da classe Task
+    def __init__(self, description, due_date, priority):
+        self.description = description
+        self.due_date = due_date
+        self.priority = priority
 
+# Abaixo está uma Função para validar a prioridade
+def validate_priority(priority):
+    # Retorna True se a prioridade estiver no intervalo de 1 a 5, caso contrário, retorna False
+    return 1 <= priority <= 5
+
+# Abaixo está o controlador das tarefas, para funções de cadastrar tarefas e ver tarefas
+class TaskController:
+    # Lista para armazenar as tarefas
+    tasks = []
+
+    # Método de classe para adicionar uma tarefa à lista de tarefas
+    @classmethod
+    def add_task(cls, task):
+        cls.tasks.append(task)
+
+    # Método de classe para visualizar todas as tarefas
+    @classmethod
+    def view_tasks(cls):
+        # Itera sobre a lista de tarefas e imprime informações sobre cada tarefa
+        for task in cls.tasks:
+            print(f"Descrição: {task.description}, Data de Vencimento: {task.due_date}, Prioridade: {task.priority}")
+
+# Abaixo é uma interface, um menu para criação das tarefas e prioridades
+class ConsoleUI:
+    # Método estático para obter entrada do usuário e criar uma nova tarefa
+    @staticmethod
+    def get_task_input():
+        description = input("Digite a descrição da tarefa: ")
+        due_date = input("Digite a Data: ")
+        priority = int(input("Digite a Prioridade (de 1 a 5): "))
+        return Task(description, due_date, priority)
+
+# Aqui é a inicialização principal do programa
+def main():
+    while True:
+        # Exibe as opções do menu
+        print("\n1. Adicionar tarefa\n2. Ver tarefas\n3. Sair")
+        # Obtém a escolha do usuário
+        choice = input("Digite sua Escolha: ")
+
+        if choice == "1":
+            # Obtém informações da tarefa a ser adicionada
+            task = ConsoleUI.get_task_input()
+            # Verifica se a prioridade é válida antes de adicionar a tarefa
+            if validate_priority(task.priority):
+                TaskController.add_task(task)
+            else:
+                print("Priorização inválida, você deve escolher algo entre 1 e 5.")
+        elif choice == "2":
+            # Exibe todas as tarefas cadastradas
+            TaskController.view_tasks()
+        elif choice == "3":
+            # Encerra o loop e sai do programa
+            break
+        else:
+            print("Escolha inválida. Tente Novamente")
+
+# Verifica se este script está sendo executado como o programa principal
 if __name__ == "__main__":
     main()
+     # Chama a função principal (main) para iniciar o programa
