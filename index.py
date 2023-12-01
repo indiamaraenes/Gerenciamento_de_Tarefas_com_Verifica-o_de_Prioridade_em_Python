033aa4572d0901822c78b7aa052f0bb962a3aaf4
marcelo <marcelomiranda200@hotmail.com>
2023-12-01 17:17:04 -0300

    correção view tasks/ exclusão de partes duplicadas

--- index.py
+++ index.py
@@ -25,50 +25,41 @@ class TaskController:
     # Função para visualizar as tarefas, depois de já cadastradas
     @classmethod
     def view_tasks(cls):
-        for task in cls.tasks:
-            print(f"Descrição: {task.description}, Data de Vencimento: {task.due_date}, Prioridade: {task.priority}")
-
-   
-
-# Abaixo é uma interface, um menu para criação das tarefas e prioridades
-class ConsoleUI:
-    @staticmethod
-    def get_task_input():
-        description = input("Digite a descrição da tarefa: ")
-        due_date = input("Digite a Data: ")
-        priority = int(input("Digite a Prioridade (de 1 a 5): "))
-        return Task(description, due_date, priority)
-
-#aqui é a inicialização principal do programa
-def main():
-    while True:
-        print("\n1. Adicionar tarefa\n2. Ver tarefas\n3. sair")
-        choice = input("Digite sua Escolha: ")
-
-        if choice == "1":
-            task = ConsoleUI.get_task_input()
-            if validate_priority(task.priority):
-                TaskController.add_task(task)
-            else:
-                print("Priorização inválida, você deve escolher algo entre 1 e 5.")
-        elif choice == "2":
-            TaskController.view_tasks()
-        elif choice == "3":
-            break
+        # Aqui é criado uma nova lista, ordenando as tarefas por prioridade
+        sorted_tasks = sorted(enumerate(cls.tasks), key=lambda x: x[1].priority, reverse=True)
+        # Aqui iteramos sobre sorted_tasks, pegando o index e as tarefas, para exibir na tela as tarefas,
+        #e seus respectivos numeros de index.
+        for index, task in sorted_tasks:
+            print(f"Descrição: {task.description}, Data de Vencimento: {task.due_date}, Prioridade: {task.priority}, INDICE DA TAREFA: {index + 1} ")
+        
+    # Função para atualizar tarefas, depois de já cadastradas
+    def update_task(self, index, updated_task):
+        adjusted_index = index - 1
+        if 0 <= adjusted_index < len(self.tasks):
+            self.tasks[adjusted_index] = updated_task
+            print("Tarefa atualizada com suceso.")
         else:
             print("Índice inválido")
 
     # Função/método para excluir tarefas, depois de ja cadastradas        
     @classmethod
-    def add_task(cls, task):
-        cls.tasks.append(task)
+    def deleted_task(cls,index):
+        adjusted_index = index - 1
+        if 0 <= adjusted_index < len(cls.tasks):
+            deleted_task = cls.tasks.pop(adjusted_index)
+            print(f"Tarefa excluida: {deleted_task.description}")
+        else:
+            print("Índice inválido.")
 
-    # Método de classe para visualizar todas as tarefas
-    @classmethod
-    def view_tasks(cls):
-        # Itera sobre a lista de tarefas e imprime informações sobre cada tarefa
-        for task in cls.tasks:
-            print(f"Descrição: {task.description}, Data de Vencimento: {task.due_date}, Prioridade: {task.priority}")
+
+
+
+
+
+
+
+
+
 
 # Abaixo é uma interface, um menu para criação das tarefas e prioridades
 class ConsoleUI:
@@ -133,7 +124,9 @@ def main():
         # Encerra o loop e sai do programa
             break
         else:
-            print("Escolha inválida. Tente Novamente")
+            print("Escolha inválida. Tente Novamente.")
+        
+      
 
 # Verifica se este script está sendo executado como o programa principal
 if __name__ == "__main__":
